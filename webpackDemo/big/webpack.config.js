b4cd053c3cd1b9c0360edbb78d57a9288fd6ba12
zhuotiantian <2252930124@qq.com>
2019-11-06 10:25:52 +0800

    整体配置

--- webpackDemo/big/webpack.config.js
+++ webpackDemo/big/webpack.config.js
@@ -1,5 +1,21 @@
-const path=require("path");
+const path = require("path");
+const StyleLintPlugin = require("stylelint-webpack-plugin");
 module.exports = {
-    mode:"development",
-    entry:"./src/index"
+  mode: "development",
+  entry: "./src/index",
+  output: {
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [{
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader']
+    }]
+  },
+  plugins: [
+    new StyleLintPlugin({
+      files: ['**/*.css']
+    })
+  ]
 }
